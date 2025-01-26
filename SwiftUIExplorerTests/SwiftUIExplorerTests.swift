7b1fdbadedf4eb6231a7de8f11752ca930c36785
Ofdikili <omerfarukdikili00@gmail.com>
2025-01-26 17:29:26 +0300

    Initial Commit

---
+++ SwiftUIExplorerTests/SwiftUIExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExplorerTests.swift
+//  SwiftUIExplorerTests
+//
+//  Created by Ömer Faruk Dikili on 26.01.2025.
+//
+
+import Testing
+@testable import SwiftUIExplorer
+
+struct SwiftUIExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
