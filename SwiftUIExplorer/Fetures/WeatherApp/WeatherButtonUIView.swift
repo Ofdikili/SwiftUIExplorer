177f07f7986a6a847b7efe6125bd1af4a00e1f5a
Ofdikili <omerfarukdikili00@gmail.com>
2025-01-27 22:11:34 +0300

    feat: Improve weather view structure
    - Created status row for weather view.
    - Separated Weather Button from Content

---
+++ SwiftUIExplorer/Fetures/WeatherApp/WeatherButtonUIView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherButtonUIView.swift
+//  SwiftUIExplorer
+//
+//  Created by Ömer Faruk Dikili on 27.01.2025.
+//
+
+import SwiftUI
+
+struct WeatherButtonUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherButtonUIView()
+}
